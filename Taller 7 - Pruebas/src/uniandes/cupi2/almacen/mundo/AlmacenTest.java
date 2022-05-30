70bb55f2f365ef28e5bacc442c664cd6dace245e
SANTIAGO AYALA CIENDUA <s.ayalac@uniandes.edu.co>
2022-05-29 22:10:02 -0500

    almacen listo

--- Taller 7 - Pruebas/src/uniandes/cupi2/almacen/mundo/AlmacenTest.java
+++ Taller 7 - Pruebas/src/uniandes/cupi2/almacen/mundo/AlmacenTest.java
@@ -31,12 +31,56 @@ class AlmacenTest {
 	@Test
 	void TestCargar () throws AlmacenException
 	{
-		try {
-			Almacen prueba = new Almacen(new File("data/datos.txt"));
-			assertNotNull(this.almacen, "No deberia de ser null");
-		} catch (AlmacenException e) {
-			e.printStackTrace();
-		}
+		assertNotNull(this.almacen, "No deberia de ser null");
+	}
+	
+	@Test
+	void TestAgregarNodo() throws AlmacenException
+	{
+		this.almacen.agregarNodo("1", "Marca", "101", "Prueba");
+		assertNotNull(this.almacen.buscarNodo("101"), "No se agrego");	
+	}
+	
+	@Test
+	void TestEliminarNodo () throws AlmacenException
+	{
+		this.almacen.agregarNodo("1", "Marca", "101", "Prueba");
+		assertNotNull(this.almacen.buscarNodo("101"), "No se agrego");
+		this.almacen.eliminarNodo("101");
+		assertNull(this.almacen.buscarNodo("101"), "No se elimino");
+	}
+	
+	@Test
+	void TestVenderProducto () throws AlmacenException
+	{
+		
+		int ventasI = this.almacen.darCategoriaRaiz().buscarProducto("31759941").darCantidadUnidadesVendidas();
+		this.almacen.venderProducto("31759941", 10);
+		int ventasF = this.almacen.darCategoriaRaiz().buscarProducto("31759941").darCantidadUnidadesVendidas();
+		boolean prueba = ventasF > ventasI;
+		
+		assertTrue(prueba);
+	}
+	
+	@Test
+	void TestBuscarNodo ()
+	{
+		assertNotNull(this.almacen.buscarNodo("1"), "No sirvio la busqueda");
+	}
+	
+	@Test 
+	void TestAgregarProducto () throws AlmacenException
+	{
+		this.almacen.agregarProducto("1111", "12345", "test", "sirve", 10);
+		assertNotNull(this.almacen.darCategoriaRaiz().buscarProducto("12345"));
+		assertEquals("test", this.almacen.darCategoriaRaiz().buscarProducto("12345").darNombre());
+	}
+	
+	@Test
+	void TestEliminarProducto() 
+	{
+		this.almacen.eliminarProducto("31759941");
+		assertNull(this.almacen.darCategoriaRaiz().buscarProducto("31759941"));
 	}
 	
 	@Test
